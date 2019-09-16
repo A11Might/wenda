01698038c421602a92975f80eca0dcaa18c15214
kohath <qihanghu@foxmail.com>
2019-09-16 10:14:09 +0800

    add sent email function

---
+++ src/main/java/com/nowcoder/async/handler/LoginExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.nowcoder.async.handler;
+
+import com.nowcoder.async.EventHandler;
+import com.nowcoder.async.EventModel;
+import com.nowcoder.async.EventType;
+import com.nowcoder.util.MailSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author 胡启航
+ * @date 2019/9/16 - 9:19
+ */
+@Component
+public class LoginExceptionHandler implements EventHandler {
+    @Autowired
+    MailSender mailSender;
+
+    @Override
+    public void doHandle(EventModel model) {
+        // xxxx判断发现这个用户登录失败
+        Map<String, Object> map = new HashMap<>();
+        map.put("username", model.getExt("username"));
+        mailSender.sendWithHTMLTemplate(model.getExt("email"), "登录失败", "mails/login_exception.html", map);
+    }
+
+    @Override
+    public List<EventType> getSupportEventTypes() {
+        return Arrays.asList(EventType.LOGIN);
+    }
+}
